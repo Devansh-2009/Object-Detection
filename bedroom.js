ea38ddffd7effc099c71c8a0a44d9f24de23a471
Devansh-2009 <87084391+Devansh-2009@users.noreply.github.com>
2022-07-02 16:41:39 +0530

    Add files via upload

--- bedroom.js
+++ bedroom.js
@@ -6,6 +6,7 @@ function setup(){
 }
 img="";
 status="";
+object=[];
 function preload(){
     img=loadImage('bedroom.jpg');
 }
@@ -14,14 +15,26 @@ function modelLoaded(){
     status=true;
     objectDetector.detect(img, gotResult);
 }
-function gotResults(error, results){
+function gotResult(error, results){
     if(error){
         console.log(error);
     }
     console.log(results);
+    objects=results;
 }
 function draw(){
     image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Object Detector";
+            fill("#FF0000");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
     fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
